2447cc57c6f35cc1fef3ef3c531b0c67e83f6477
leacrepin <leacrepin@hotmail.fr>
2018-07-13 16:38:03 +0200

    2.3

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -5,7 +5,9 @@
 namespace OC\PlatformBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class AdvertController extends Controller
 {
@@ -23,16 +25,15 @@ class AdvertController extends Controller
     return new Response($content);
   }
 
-  public function viewAction($id)
+  public function viewAction($id, Request $request)
   {
-    // $id vaut 5 si l'on a appelé l'URL /platform/advert/5
+    // On récupère notre paramètre tag
+    $tag = $request->query->get('tag');
 
-    // Ici, on récupèrera depuis la base de données
-    // l'annonce correspondant à l'id $id.
-    // Puis on passera l'annonce à la vue pour
-    // qu'elle puisse l'afficher
-
-    return new Response("Affichage de l'annonce d'id : ".$id);
+    return $this->render('OCPlatformBundle:Advert:view.html.twig', array(
+      'id'  => $id,
+      'tag' => $tag,
+    ));
   }
 
   public function viewSlugAction($slug, $year, $format)
@@ -47,7 +48,8 @@ class AdvertController extends Controller
     {
         $url = $this->get('router')->generate(
             'oc_platform_view', // 1er argument : le nom de la route
-            array('id' => 5)    // 2e argument : les valeurs des paramètres
+            array('id' => 5),    // 2e argument : les valeurs des paramètres
+            UrlGeneratorInterface::ABSOLUTE_URL
         );
         $content = $this->get('templating')->render('OCPlatformBundle:Advert:index.html.2.twig', array('url' => $url, 'id' => 5));
     
